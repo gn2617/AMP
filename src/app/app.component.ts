20e8ddee609f247bb106aa796b799c060f38fe76
Gustavo Nunez <m3ac-nunezg@m3as.com>
2018-08-24 15:21:54 -0400

    this is the work for module 6 video 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
 selector : 'pm-root',
+template : `
+<div>
+  <h1>
+    {{pageTitle}}
+  </h1>
+   <pm-products></pm-products>
+</div>
+`,
+// styleUrls: []
 
 })
 export class AppComponent {
